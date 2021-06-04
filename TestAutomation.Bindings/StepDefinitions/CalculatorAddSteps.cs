c04ef9fa776e0edb1ec3e0fa31db8eda1e4db857
Eleana Morgan <Elle1Morgan@outlook.com>
2021-06-04 12:04:09 +0000

    Created step definitions for calculator

---
+++ TestAutomation.Bindings/StepDefinitions/CalculatorAddSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TestAutomation.Bindings.StepDefinitions
+{
+    [Binding]
+    public class CalculatorAddSteps
+    {
+        [Given(@"the calulator is on")]
+        public void GivenTheCalulatorIsOn()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I add '(.*)' and '(.*)'")]
+        public void WhenIAddAnd(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"I add '(.*)' and '(.*)'")]
+        public void WhenIAddAnd(Decimal p0, Decimal p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be '(.*)'")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should be '(.*)'")]
+        public void ThenTheResultShouldBe(Decimal p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
---
+++ TestAutomation/Features/CalculatorAdd.feature
@@ -0,0 +1,21 @@
+﻿@add
+Feature: CalculatorAdd
+	In order to avoid silly mistakes
+	As a math idiot
+	I want to be told the sum of two numbers
+
+
+Scenario: Add two numbers
+	Given the calulator is on
+	When I add '50' and '70'
+	Then the result should be '120'
+
+Scenario: Add two decimal numbers
+	Given the calulator is on
+	When I add '2.2' and '4.4'
+	Then the result should be '6.6'
+
+Scenario: Add two negative numbers
+	Given the calulator is on
+	When I add '-2.2' and '-4.4'
+	Then the result should be '6.6'
