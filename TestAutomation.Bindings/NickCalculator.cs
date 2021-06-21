ce11068d54d52f63a9a300da15328ac2bd399044
nickdrury <nickdrury@tfl.gov.uk>
2021-06-21 13:00:26 +0100

    Create calculator class

---
+++ TestAutomation.Bindings/NickCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAutomation.Bindings
+{
+    public class NickCalculator
+    {
+        public int Add()
+        {
+            return 0;
+        }
+    }
+}
