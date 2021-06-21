f5c921ac89a81de22c9668da99c1e3bc58fed164
Kate Chan <KateChan@tfl.gov.uk>
2021-06-21 12:01:46 +0000

    WIP creating cal class

---
+++ TestAutomation.Bindings/ProjectTraining.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestAutomation.Bindings
+{
+    public class ProjectTraining
+    {
+        public int Add()
+        { return 0; }
+     
+        }
+}
