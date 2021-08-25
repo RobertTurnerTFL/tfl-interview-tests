a6cf53047f4c35473cd51ba25807c35187efff29
subamahadev <subamahadev@gmail.com>
2021-08-26 00:40:16 +0100

    Adding Stepdefinitions

---
+++ TestAutomation.Bindings/StepDefinitions/JourneyPlanner.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+using TestAutomation.Bindings.Contexts;
+using TestAutomation.PageObjects.Factories;
+
+namespace TestAutomation.Bindings.StepDefinitions
+{
+    [Binding]
+    public class JourneyPlanner : BaseSteps
+    {
+        public JourneyPlanner(IPageObjectFactory pageObjectFactory, PageContext pageContext) : base(pageObjectFactory, pageContext)
+        {
+        }
+
+        [StepDefinition(@"user is on the TfL home page")]
+        public void GivenUserIsOnTheTfLHomePage()
+        {
+            PageContext.TflPage = PageObjectFactory.CreateTflPage().Navigate();
+        }
+
+        [StepDefinition(@"the Tfl home page has loaded successfully")]
+        public void GivenTheTflHomePageHasLoadedSuccessfully()
+        {
+            PageObjectFactory.CreateTflPage().AcceptCookies();
+            PageObjectFactory.CreateTflPage().WaitForPageElements();
+        }
+
+        [StepDefinition(@"user plans a journey from '(.*)' to '(.*)'")]
+        public void WhenUserPlansAJourneyFromTo(string FromStation, string ToStation)
+        {
+            PageObjectFactory.CreateTflPage().EnterJourneyDetails("FromStation", FromStation);
+            PageObjectFactory.CreateTflPage().EnterJourneyDetails("ToStation", ToStation);
+            PageContext.JourneyResultsPage = PageObjectFactory.CreateTflPage().ClickPlanMyJourney();
+        }
+
+        [StepDefinition(@"user enters text that does not match a station name into the journey planner")]
+        [StepDefinition(@"user plans a journey")]
+        public void GivenUserPlansAJourney(Table table)
+        {
+            var JourneyDetails = table.CreateInstance<JourneyDetails>();
+            PageObjectFactory.CreateTflPage().EnterJourneyDetails("FromStation",JourneyDetails.FromStation);
+            PageObjectFactory.CreateTflPage().EnterJourneyDetails("ToStation", JourneyDetails.ToStation);
+        }
+
+
+        [StepDefinition(@"user plans a journey from '(.*)'")]
+        public void GivenUserPlansAJourneyFrom(string FromStation)
+        {
+            PageObjectFactory.CreateTflPage().EnterJourneyDetails("FromStation", FromStation);
+        }
+        
+        [StepDefinition(@"user plans a journey to '(.*)'")]
+        public void GivenUserPlansAJourneyTo(string ToStation)
+        {
+            PageObjectFactory.CreateTflPage().EnterJourneyDetails("ToStation", ToStation);
+        }
+        
+        [StepDefinition(@"user clicks Plan my journey")]
+        public void WhenUserClicksPlanMyJourney()
+        {
+            PageContext.JourneyResultsPage = PageObjectFactory.CreateTflPage().ClickPlanMyJourney();
+        }
+
+        [StepDefinition(@"user should remain on the TFL Home Page when user clicks on Plan my journey")]
+        public void GivenUserShouldRemainOnTheTFLHomePageWhenUserClicksOnPlanMyJourney()
+        {
+            PageContext.TflPage = PageObjectFactory.CreateTflPage().ClickPlanMyJourneyInvalidFromOrToStation();
+        }
+
+
+        [StepDefinition(@"user should be presented with the Journey Results page with the correct summary")]
+        public void ThenUserShouldBePresentedWithTheJourneyResultsPageWithTheCorrectSummary()
+        {
+            PageObjectFactory.CreateJourneyResultsPage().LastBreadCrumbText.Should().Be("Journey results");
+            PageObjectFactory.CreateJourneyResultsPage().ValidateJourneyResults();
+            PageObjectFactory.CreateJourneyResultsPage().ValidateIfMoreThanOneLocationMatchingFound();
+        }
+
+        [StepDefinition(@"user can see the fastest route")]
+        public void ThenUserCanSeeTheFastestRoute()
+        {
+            PageObjectFactory.CreateJourneyResultsPage().ValidateIfFastestRouteIsDisplayed();
+        }
+
+        [StepDefinition(@"user should be presented with the Journey Results page with an error message")]
+        public void ThenUserShouldBePresentedWithTheJourneyResultsPageWithAnErrorMessage()
+        {
+            PageObjectFactory.CreateJourneyResultsPage().ErrorMessageDisplayedNoResultsFound().Should()
+                .Be(JourneyDetails.InvalidValuesToBothFields);
+        }
+
+        [StepDefinition(@"user sees an error message telling them that the to field is required")]
+        public void ThenUserSeesAnErrorMessageTellingThemThatTheToFieldIsRequired()
+        {
+            PageObjectFactory.CreateTflPage().GetErrorMessageTo().Should().Be(JourneyDetails.ToStationErrorMessage);
+        }
+
+        [StepDefinition(@"user sees an error message telling them that the from field is required")]
+        public void ThenUserSeesAnErrorMessageTellingThemThatTheFromFieldIsRequired()
+        {
+            PageObjectFactory.CreateTflPage().GetErrorMessageFrom().Should().Be(JourneyDetails.FromStationErrorMessage);
+        }
+
+        [StepDefinition(@"user changes the destination to '(.*)'")]
+        public void WhenUserChangesTheDestinationTo(string ToStation)
+        {
+            PageContext.JourneyResultsPage = PageContext.JourneyResultsPage.ClickEditJourney();
+            PageContext.JourneyResultsPage.EditJourneyDetails("ToStation",ToStation);
+        }
+        [StepDefinition(@"user clicks Plan my journey in Edit Journey")]
+        public void WhenUserClicksPlanMyJourneyInEditJourney()
+        {
+            PageContext.JourneyResultsPage = PageObjectFactory.CreateJourneyResultsPage().ClickPlanMyJourney();
+
+        }
+
+
+    }
+}
---
+++ TestAutomation.Framework/Helpers/Common.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using FluentAssertions;
+
+namespace TestAutomation.Framework.Helpers
+{
+    public class Common
+    {
+        public static void compareDataTables(DataTable master, DataTable subset)
+        {
+            for (int i = 0; i < subset.Rows.Count; i++)
+            {
+                for (int c = 0; c < subset.Columns.Count; c++)
+                {
+                    subset.Rows[i][c].ToString().Should().Be(master.Rows[i][c].ToString());
+                }
+            }
+        }
+
+        public static int timeInMins(string Duration)
+        {
+            if (Duration.Contains("hrs") && Duration.Contains("mins"))
+            {
+                return int.Parse(Duration.Split("hrs")[0].Trim()) * 60 +
+                       int.Parse(Duration.Split("hrs")[1].Split("mins")[0].Trim());
+            }
+            else if (Duration.Contains("hrs"))
+            {
+                return (int.Parse(Duration.Split("hrs")[0].Trim()) * 60);
+            }
+            else if (Duration.Contains("mins"))
+            {
+                return int.Parse(Duration.Split("mins")[0].Trim());
+            }
+
+            return 0;
+        }
+
+    }
+}
