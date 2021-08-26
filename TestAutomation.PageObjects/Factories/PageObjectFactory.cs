d9303f86cb1d54a8bfefd48b4b0c1dd5caff516c
subamahadev <subamahadev@gmail.com>
2021-08-26 01:02:07 +0100

    Add files via upload

--- TestAutomation.PageObjects/Factories/PageObjectFactory.cs
+++ TestAutomation.PageObjects/Factories/PageObjectFactory.cs
@@ -1,4 +1,5 @@
-﻿using TestAutomation.Framework.Interfaces;
+﻿using TechTalk.SpecFlow;
+using TestAutomation.Framework.Interfaces;
 using TestAutomation.PageObjects.Pages;
 
 namespace TestAutomation.PageObjects.Factories
@@ -7,14 +8,28 @@ namespace TestAutomation.PageObjects.Factories
     {
         private readonly IWebDriverManager _webDriverManager;
 
-        public PageObjectFactory(IWebDriverManager webDriverManager)
+        private ScenarioContext _scenarioContext;
+
+        public PageObjectFactory(IWebDriverManager webDriverManager,ScenarioContext scenarioContext)
         {
             _webDriverManager = webDriverManager;
+            _scenarioContext = scenarioContext;
         }
 
         public LoginPage CreateLoginPage()
         {
             return new LoginPage(_webDriverManager);
         }
+
+        public TflPage CreateTflPage()
+        {
+            return new TflPage(_webDriverManager,_scenarioContext);
+        }
+
+        public JourneyResultsPage CreateJourneyResultsPage()
+        {
+            return new JourneyResultsPage(_webDriverManager,_scenarioContext);
+        }
+
     }
 }
